9466ef0ee4e118d712c9bb4032c3278654d2df04
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-07-21 09:37:20 +0300

    new arrivals

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php
 get_header()
 	?>
-<!--populer-products start -->
-<section id="populer-products" class="populer-products">
-	<div class="container">
-		<div class="populer-products-content">
-			<div class="row">
-				<?php
-				$args = array(
-					'post_type' => 'populer-products',
-					'posts_per_page' => -1,
-				);
 
-				$populer_query = new WP_Query($args);
-				$i = 1;
+<?php
+$args = array(
+	'post_type' => 'populer-products',
+	'posts_per_page' => -1,
+);
 
-				if ($populer_query->have_posts()):
-					while ($populer_query->have_posts()):
+$populer_query = new WP_Query($args);
+$i = 1;
+
+if ($populer_query->have_posts()): ?>
+	<!--populer-products start -->
+	<section id="populer-products" class="populer-products">
+		<div class="container">
+			<div class="populer-products-content">
+				<div class="row">
+					<?php while ($populer_query->have_posts()):
 						$populer_query->the_post();
 						$post_image_id = get_post_meta(get_the_ID(), 'product_image', true);
 						$product_image = wp_get_attachment_image_src($post_image_id, 'full')[0];
@@ -79,209 +80,80 @@ get_header()
 								</div>
 							</div>
 						<?php endif ?>
-				<?php $i += 1;
+					<?php $i += 1;
 					endwhile;
-					wp_reset_postdata();
-				endif;
-				?>
+					wp_reset_postdata(); ?>
+				</div>
 			</div>
-		</div>
-	</div><!--/.container-->
+		</div><!--/.container-->
 
-</section><!--/.populer-products-->
-<!--populer-products end-->
+	</section><!--/.populer-products-->
+	<!--populer-products end-->
+<?php endif;
+?>
 
-<!--new-arrivals start -->
-<section id="new-arrivals" class="new-arrivals">
-	<div class="container">
-		<div class="section-header">
-			<h2>new arrivals</h2>
-		</div><!--/.section-header-->
-		<div class="new-arrivals-content">
-			<div class="row">
-				<div class="col-md-3 col-sm-4">
-					<div class="single-new-arrival">
-						<div class="single-new-arrival-bg">
-							<img src="<?php bloginfo('template_url') ?>/assets/images/collection/arrivals1.png"
-								alt="new-arrivals images">
-							<div class="single-new-arrival-bg-overlay"></div>
-							<div class="sale bg-1">
-								<p>sale</p>
-							</div>
-							<div class="new-arrival-cart">
-								<p>
-									<span class="lnr lnr-cart"></span>
-									<a href="#">add <span>to </span> cart</a>
-								</p>
-								<p class="arrival-review pull-right">
-									<span class="lnr lnr-heart"></span>
-									<span class="lnr lnr-frame-expand"></span>
-								</p>
-							</div>
-						</div>
-						<h4><a href="#">wooden chair</a></h4>
-						<p class="arrival-product-price">$65.00</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-4">
-					<div class="single-new-arrival">
-						<div class="single-new-arrival-bg">
-							<img src="<?php bloginfo('template_url') ?>/assets/images/collection/arrivals2.png"
-								alt="new-arrivals images">
-							<div class="single-new-arrival-bg-overlay"></div>
-							<div class="sale bg-2">
-								<p>sale</p>
-							</div>
-							<div class="new-arrival-cart">
-								<p>
-									<span class="lnr lnr-cart"></span>
-									<a href="#">add <span>to </span> cart</a>
-								</p>
-								<p class="arrival-review pull-right">
-									<span class="lnr lnr-heart"></span>
-									<span class="lnr lnr-frame-expand"></span>
-								</p>
-							</div>
-						</div>
-						<h4><a href="#">single armchair</a></h4>
-						<p class="arrival-product-price">$80.00</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-4">
-					<div class="single-new-arrival">
-						<div class="single-new-arrival-bg">
-							<img src="<?php bloginfo('template_url') ?>/assets/images/collection/arrivals3.png"
-								alt="new-arrivals images">
-							<div class="single-new-arrival-bg-overlay"></div>
-							<div class="new-arrival-cart">
-								<p>
-									<span class="lnr lnr-cart"></span>
-									<a href="#">add <span>to </span> cart</a>
-								</p>
-								<p class="arrival-review pull-right">
-									<span class="lnr lnr-heart"></span>
-									<span class="lnr lnr-frame-expand"></span>
+<?php
+$args = array(
+	'post_type' => 'new-arrivals',
+	'posts_per_page' => 8,
+	'order' => 'ASC'
+);
+
+$new_arrivals_query = new WP_Query($args);
+
+if ($new_arrivals_query->have_posts()): ?>
+	<!--new-arrivals start -->
+	<section id="new-arrivals" class="new-arrivals">
+		<div class="container">
+			<div class="section-header">
+				<h2>new arrivals</h2>
+			</div><!--/.section-header-->
+			<div class="new-arrivals-content">
+				<div class="row">
+					<?php while ($new_arrivals_query->have_posts()):
+						$new_arrivals_query->the_post();
+						$post_image_id = get_post_meta(get_the_ID(), 'item_image', true);
+						$item_image = wp_get_attachment_image_src($post_image_id, 'full')[0];
+						$item_title = get_post_meta(get_the_ID(), 'item_title', true);
+						$item_price = get_post_meta(get_the_ID(), 'item_price', true);
+						$on_sale = get_post_meta(get_the_ID(), 'on_sale', true); ?>
+
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="<?php echo $item_image ?>" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<?php
+									if ($on_sale) { ?>
+										<div class="sale bg-<?php echo mt_rand(1, 2) ?>">
+											<p>sale</p>
+										</div>
+									<?php }
+									?>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">
+										<?php echo $item_title ?>
+									</a></h4>
+								<p class="arrival-product-price">
+									<?php echo $item_price ?>
 								</p>
 							</div>
 						</div>
-						<h4><a href="#">wooden armchair</a></h4>
-						<p class="arrival-product-price">$40.00</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-4">
-					<div class="single-new-arrival">
-						<div class="single-new-arrival-bg">
-							<img src="<?php bloginfo('template_url') ?>/assets/images/collection/arrivals4.png"
-								alt="new-arrivals images">
-							<div class="single-new-arrival-bg-overlay"></div>
-							<div class="sale bg-1">
-								<p>sale</p>
-							</div>
-							<div class="new-arrival-cart">
-								<p>
-									<span class="lnr lnr-cart"></span>
-									<a href="#">add <span>to </span> cart</a>
-								</p>
-								<p class="arrival-review pull-right">
-									<span class="lnr lnr-heart"></span>
-									<span class="lnr lnr-frame-expand"></span>
-								</p>
-							</div>
-						</div>
-						<h4><a href="#">stylish chair</a></h4>
-						<p class="arrival-product-price">$100.00</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-4">
-					<div class="single-new-arrival">
-						<div class="single-new-arrival-bg">
-							<img src="<?php bloginfo('template_url') ?>/assets/images/collection/arrivals5.png"
-								alt="new-arrivals images">
-							<div class="single-new-arrival-bg-overlay"></div>
-							<div class="new-arrival-cart">
-								<p>
-									<span class="lnr lnr-cart"></span>
-									<a href="#">add <span>to </span> cart</a>
-								</p>
-								<p class="arrival-review pull-right">
-									<span class="lnr lnr-heart"></span>
-									<span class="lnr lnr-frame-expand"></span>
-								</p>
-							</div>
-						</div>
-						<h4><a href="#">modern chair</a></h4>
-						<p class="arrival-product-price">$120.00</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-4">
-					<div class="single-new-arrival">
-						<div class="single-new-arrival-bg">
-							<img src="<?php bloginfo('template_url') ?>/assets/images/collection/arrivals6.png"
-								alt="new-arrivals images">
-							<div class="single-new-arrival-bg-overlay"></div>
-							<div class="sale bg-1">
-								<p>sale</p>
-							</div>
-							<div class="new-arrival-cart">
-								<p>
-									<span class="lnr lnr-cart"></span>
-									<a href="#">add <span>to </span> cart</a>
-								</p>
-								<p class="arrival-review pull-right">
-									<span class="lnr lnr-heart"></span>
-									<span class="lnr lnr-frame-expand"></span>
-								</p>
-							</div>
-						</div>
-						<h4><a href="#">mapple wood dinning table</a></h4>
-						<p class="arrival-product-price">$140.00</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-4">
-					<div class="single-new-arrival">
-						<div class="single-new-arrival-bg">
-							<img src="<?php bloginfo('template_url') ?>/assets/images/collection/arrivals7.png"
-								alt="new-arrivals images">
-							<div class="single-new-arrival-bg-overlay"></div>
-							<div class="sale bg-2">
-								<p>sale</p>
-							</div>
-							<div class="new-arrival-cart">
-								<p>
-									<span class="lnr lnr-cart"></span>
-									<a href="#">add <span>to </span> cart</a>
-								</p>
-								<p class="arrival-review pull-right">
-									<span class="lnr lnr-heart"></span>
-									<span class="lnr lnr-frame-expand"></span>
-								</p>
-							</div>
-						</div>
-						<h4><a href="#">arm chair</a></h4>
-						<p class="arrival-product-price">$90.00</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-4">
-					<div class="single-new-arrival">
-						<div class="single-new-arrival-bg">
-							<img src="<?php bloginfo('template_url') ?>/assets/images/collection/arrivals8.png"
-								alt="new-arrivals images">
-							<div class="single-new-arrival-bg-overlay"></div>
-							<div class="new-arrival-cart">
-								<p>
-									<span class="lnr lnr-cart"></span>
-									<a href="#">add <span>to </span> cart</a>
-								</p>
-								<p class="arrival-review pull-right">
-									<span class="lnr lnr-heart"></span>
-									<span class="lnr lnr-frame-expand"></span>
-								</p>
-							</div>
-						</div>
-						<h4><a href="#">wooden bed</a></h4>
-						<p class="arrival-product-price">$140.00</p>
-					</div>
-				</div>
+
+					<?php endwhile;
+endif;
+wp_reset_postdata(); ?>
+
 			</div>
 		</div>
 	</div><!--/.container-->
