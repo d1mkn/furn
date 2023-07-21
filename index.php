7bbbc7d70d38e920d71491dfb941b940504e1962
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-07-21 10:47:07 +0300

    featured products

--- index.php
+++ index.php
@@ -168,7 +168,6 @@ $args = array(
 );
 
 $collection_query = new WP_Query($args);
-$i = 1;
 
 if ($collection_query->have_posts()): ?>
 	<!--sofa-collection start -->
@@ -202,7 +201,6 @@ if ($collection_query->have_posts()): ?>
 						</div>
 					</div>
 				</div><!--/.sofa-collection-->
-				<?php $i += 1 ?>
 			<?php endwhile;
 endif ?>
 	</div><!--/.collection-carousel-->
@@ -210,82 +208,63 @@ endif ?>
 </section><!--/.sofa-collection-->
 <!--sofa-collection end -->
 
-<!--feature start -->
-<section id="feature" class="feature">
-	<div class="container">
-		<div class="section-header">
-			<h2>featured products</h2>
-		</div><!--/.section-header-->
-		<div class="feature-content">
-			<div class="row">
-				<div class="col-sm-3">
-					<div class="single-feature">
-						<img src="<?php bloginfo('template_url') ?>/assets/images/features/f1.jpg" alt="feature image">
-						<div class="single-feature-txt text-center">
-							<p>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<span class="spacial-feature-icon"><i class="fa fa-star"></i></span>
-								<span class="feature-review">(45 review)</span>
-							</p>
-							<h3><a href="#">designed sofa</a></h3>
-							<h5>$160.00</h5>
+<?php
+$args = array(
+	'post_type' => 'featured-products',
+	'posts_per_page' => -1,
+	'order' => 'ASC'
+);
+
+$featured_products_query = new WP_Query($args);
+
+if ($featured_products_query->have_posts()): ?>
+	<!--feature start -->
+	<section id="feature" class="feature">
+		<div class="container">
+			<div class="section-header">
+				<h2>featured products</h2>
+			</div><!--/.section-header-->
+			<div class="feature-content">
+				<div class="row">
+
+					<?php while ($featured_products_query->have_posts()):
+						$featured_products_query->the_post();
+						$post_image_id = get_post_meta(get_the_ID(), 'product_image', true);
+						$product_image = wp_get_attachment_image_src($post_image_id, 'full')[0];
+						$product_title = get_post_meta(get_the_ID(), 'product_title', true);
+						$product_description = get_post_meta(get_the_ID(), 'product_description', true);
+						$product_rating = get_post_meta(get_the_ID(), 'product_rating', true);
+						$reviews_count = get_post_meta(get_the_ID(), 'reviews_count', true);
+						$product_price = get_post_meta(get_the_ID(), 'product_price', true); ?>
+						<div class="col-sm-3">
+							<div class="single-feature">
+								<img src="<?php echo $product_image ?>" alt="feature image">
+								<div class="single-feature-txt text-center">
+									<p>
+										<?php
+										for ($i = 1; $i <= $product_rating; $i++) {
+											echo '<i class="fa fa-star"></i>';
+										}
+										$total_rating = 5;
+										$stars = $total_rating - $product_rating;
+										for ($i = 0; $i < $stars; $i++) {
+											echo '<span class="spacial-feature-icon"><i class="fa fa-star"></i></span>';
+										}
+										?>
+										<span class="feature-review">(
+											<?php echo $reviews_count ?> review)
+										</span>
+									</p>
+									<h3><a href="#">
+											<?php echo $product_title ?>
+										</a></h3>
+									<h5><?php echo $product_price ?></h5>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="single-feature">
-						<img src="<?php bloginfo('template_url') ?>/assets/images/features/f2.jpg" alt="feature image">
-						<div class="single-feature-txt text-center">
-							<p>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<span class="spacial-feature-icon"><i class="fa fa-star"></i></span>
-								<span class="feature-review">(45 review)</span>
-							</p>
-							<h3><a href="#">dinning table </a></h3>
-							<h5>$200.00</h5>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="single-feature">
-						<img src="<?php bloginfo('template_url') ?>/assets/images/features/f3.jpg" alt="feature image">
-						<div class="single-feature-txt text-center">
-							<p>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<span class="spacial-feature-icon"><i class="fa fa-star"></i></span>
-								<span class="feature-review">(45 review)</span>
-							</p>
-							<h3><a href="#">chair and table</a></h3>
-							<h5>$100.00</h5>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="single-feature">
-						<img src="<?php bloginfo('template_url') ?>/assets/images/features/f4.jpg" alt="feature image">
-						<div class="single-feature-txt text-center">
-							<p>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<span class="spacial-feature-icon"><i class="fa fa-star"></i></span>
-								<span class="feature-review">(45 review)</span>
-							</p>
-							<h3><a href="#">modern arm chair</a></h3>
-							<h5>$299.00</h5>
-						</div>
-					</div>
-				</div>
+						<?php
+					endwhile;
+endif; ?>
 			</div>
 		</div>
 	</div><!--/.container-->
